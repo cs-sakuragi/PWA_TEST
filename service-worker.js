9d107e22baea17397a52327cfa94d03b6e77037a
unknown <atsushi.sakuragi@central-soft.co.jp>
2018-08-17 15:31:16 +0900

    test8

--- service-worker.js
+++ service-worker.js
@@ -5,3 +5,17 @@ self.addEventListener('install', function(e) {
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
 });
+
+self.addEventListener('push', (event) => {
+  console.info('push', event);
+
+  const message = event.data ? event.data.text() : 'NG';
+
+  event.waitUntil(
+      self.registration.showNotification('Push Notification Title', {
+          body: message,
+          icon: '',
+          tag: 'push-notification-tag'
+      })
+  );
+});
